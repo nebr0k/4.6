702dc16006e694f5df882eba88bc2a376d2e6f91
nebr0k <90468237+nebr0k@users.noreply.github.com>
2021-10-25 21:34:57 +0300

    Add files via upload

---
+++ 4.6.cpp
@@ -0,0 +1,65 @@
+﻿//Kоцюба Остап в-4
+#include <iostream>
+#include <cmath>
+#include <iomanip>
+using namespace std;
+int main() {
+     double P, S;
+	int j, i;
+	P = 1;
+	j = 1;
+	while (j <= 15)
+	{
+		S = 0;
+		i = j;
+		while (i <= j*j)
+		{
+			S += (1. * i * i);
+			i++;
+		}
+		P *= sqrt(S);
+		j++;
+	}
+	cout << P << endl;
+	P = 1;
+	j = 1;
+	do
+	{
+		S = 0;
+		i = j;
+		do
+		{
+
+			S += (1. * i * i);
+			i++;
+		} while (i <= j*j);
+		P *= sqrt(S) ;
+		j++;
+	} while ( j<= 15);
+	cout << P << endl;
+	P = 1;
+	for (j = 1; j <= 15; j++)
+	{
+		S = 0;
+		for (i = j; i <= j*j; i++)
+		{
+			S += (1. * i * i);
+		}
+		P *= sqrt (S) ;
+	}
+	cout << P << endl;
+	P = 1;
+	for (j = 15; j >= 1; j--)
+
+	{
+		S = 0;
+		for (i = j*j; i >= j; i--)
+
+		{
+			S += (1. * i * i);
+		}
+		P *= sqrt(S);
+	}
+	cout << P << endl;
+	return 0;
+}
